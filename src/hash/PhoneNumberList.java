9af3ae3c77b3129e30c3e102b2e5581b5cfd8855
JiHyun Jang <gihyun0818@gmail.com>
2025-07-28 19:15:30 +0900

    1.프로그래머스 > hash > 의상 2.프로그래머스 > hash > 전화번호 목록

---
+++ src/hash/PhoneNumberList.java
@@ -0,0 +1,22 @@
+package hash;
+
+import java.util.Arrays;
+
+public class PhoneNumberList {
+    public static void main(String[] args) {
+        boolean result = solution(new String[]{"12","111","88","123","1235"});
+        System.out.println("result = " + result);
+    }
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i + 1].startsWith(phone_book[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
